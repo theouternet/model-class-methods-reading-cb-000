f243776039d2550dd44d4c9bd4acb4a5729c96e6
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-04 00:38:37 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,9 +10,9 @@ def index
     @posts = Post.by_author(params[:author])
   elsif !params[:date].blank?
     if params[:date] == "Today"
-      @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
+      @posts = Post.from_today
     else
-      @posts = Post.where("created_at <?", Time.zone.today.beginning_of_day)
+      @posts = Post.old_news
     end
   else
     # if no filters are applied, show all posts
